91de5b6225fef92a61e187543a0e27900c846a0a
imane chaaoula <chaaoula.imane.0@gmail.com>
2024-03-08 23:19:59 +0100

    im

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 
 import App from './App';
-// import App from './Components/exemple';
+// import App from './Components/Copm';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
